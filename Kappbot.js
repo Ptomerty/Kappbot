34ff4ca779dcb208760903315a0cd05ad4af0ac9
Ptomerty <Ptomerty@users.noreply.github.com>
2017-06-14 22:18:37 -0400

    const and let everywhere

--- Kappbot.js
+++ Kappbot.js
@@ -33,72 +33,68 @@ return readFile('./appstate.json', 'utf8')
 
 		api.listen((err, message) => {
 			if (err) return console.warn(err);
-			
+
 			function cleanMessage(msg) {
 				return msg
 					.replace(/[^\w\s]|_/g, "")
 					.replace(/\s+/g, " ")
 					.toLowerCase();
 			}
-			
+
 			function isGlobalEmote(word) {
 				return (globalEmotes.emotes[word] != null);
 			}
-			
+
 			function isSubEmote(word) {
 				return (subEmotes.emotes.find(obj => obj.code === word) != null);
 			}
-			
+
 			function isBTTVEmote(word) {
 				return (bttvEmotes.emotes.find(obj => obj.code === word) != null);
 			}
-			
+
 			function isCustomEmote(word) {
 				return (customEmotes.emotes[word] != null);
 			}
 
 			function checkForCommands(message) {
-				var split = message.body.split(" ");
+				const split = message.body.split(" ");
 
 				if (message.body === '!id') {
 					api.sendMessage("Your ID is " + message.senderID, message.threadID);
 				} else if (message.body === '!commands') {
-					var send = "Commands: " + commands.join(', ');
-					api.sendMessage(send, message.threadID);
+					const response = "Commands: " + commands.join(', ');
+					api.sendMessage(response, message.threadID);
 				} else if (message.body === '!modcommands') {
-					var send = "Mod Commands: " + modcommands.join(', ');
-					api.sendMessage(send, message.threadID);
+					const response = "Mod Commands: " + modcommands.join(', ');
+					api.sendMessage(response, message.threadID);
 				} else if (message.body === '!customlist') {
-					var send = "Custom emote list: ";
-					Object.keys(customEmotes.emotes).forEach(function(key) {
-						send += key + ', ';
-					});
-					send = send.substring(0, send.length - 2);
-					api.sendMessage(send, message.threadID);
+					const response = "Custom emote list: " + Object.keys(customEmotes.emotes).join(', ');
+					api.sendMessage(response, message.threadID);
 				} else if (message.body === '!ping') {
 					api.sendMessage("pong", message.threadID);
 				} else if (message.body === '!threadID') {
 					api.sendMessage(message.threadID + "", message.threadID);
 				} else if (split[0] === '!echo' && split.length > 1) {
-					var send = split.slice(1).join(" ");
-					api.sendMessage(send, message.threadID);
+					const response = split.slice(1).join(" ");
+					api.sendMessage(response, message.threadID);
 				} else if (message.body === '!modlist' && split.length === 1) {
-					var send = "Mods: ";
+					let response = "Mods: ";
 					api.getUserInfo(modlist, (err, ret) => {
 						if (err) return console.error(err);
 						for (var prop in ret) {
-							send += ret[prop].name + ", ";
+							response += ret[prop].name + ", ";
 						}
-						send = send.substring(0, send.length - 2);
-						api.sendMessage(send, message.threadID);
+						response = response.substring(0, response.length - 2);
+						api.sendMessage(response, message.threadID);
 					});
 				} else if (modlist.includes(message.senderID)) {
 					//note that addemote and delemote are broken until readfile support
 					if (split[0] === '!addemote' && split.length === 4) {
-						var emotename = split[1].toLowerCase();
-						var url = "http://" + split[2] + "/" + split[3];
+						const emotename = split[1].toLowerCase();
+						const url = "http://" + split[2] + "/" + split[3];
 						customEmotes.emotes[emotename] = '';
-						var emotefilename = __dirname + '/emotes/' + emotename + '.png';
+						const emotefilename = __dirname + '/emotes/' + emotename + '.png';
 						return emotefxn.downloadImage(url, emotefilename)
 							.then(() => {
 								api.sendMessage("Emote added!", message.threadID);
@@ -107,9 +103,9 @@ return readFile('./appstate.json', 'utf8')
 								console.error("error while adding emote!");
 							});
 					} else if (split[0] === '!delemote' && split.length === 2) {
-						var emotename = split[1];
+						const emotename = split[1];
 						delete customEmotes.emotes[emotename];
-						var emotefilename = __dirname + '/emotes/' + emotename + '.png';
+						const emotefilename = __dirname + '/emotes/' + emotename + '.png';
 
 						return unlink(emotefilename)
 							.then(() => {
@@ -120,7 +116,7 @@ return readFile('./appstate.json', 'utf8')
 							});
 
 					} else if (split[0] === '!mod' && split.length === 3) {
-						var name = split[1] + " " + split[2];
+						const name = split[1] + " " + split[2];
 						api.getUserID(name, (err, data) => {
 							if (err) {
 								api.sendMessage("Lookup failed, exiting.", message.threadID);
@@ -130,7 +126,7 @@ return readFile('./appstate.json', 'utf8')
 							api.sendMessage("Mod successful!", message.threadID);
 						});
 					} else if (split[0] === '!demod' && split.length === 3) {
-						var name = split[1] + " " + split[2];
+						const name = split[1] + " " + split[2];
 						api.getUserID(name, (err, data) => {
 							if (err) {
 								api.sendMessage("Lookup failed, exiting.", message.threadID);
@@ -142,18 +138,18 @@ return readFile('./appstate.json', 'utf8')
 							}
 						});
 					} else if (split[0] === '!echothread' && split.length > 2) {
-						var send = split.slice(2).join(" ");
-						api.sendMessage(send.substring(0, send.length - 1), split[1]);
+						const response = split.slice(2).join(" ");
+						api.sendMessage(response, split[1]);
 					}
 				} else {
 					//eventually move to another function?
 					let cleanedMsg = cleanMessage(message.body);
 					let splitWords = cleanedMsg.split(" ");
-					
+
 					return Promise.filter(splitWords, (word) => {
 						return (isGlobalEmote(word) || isSubEmote(word) || isBTTVEmote(word) || isCustomEmote(word));
 					}).then((emoteWords) => {
-						return emoteWords.slice(0,5); //only return 5 in order
+						return emoteWords.slice(0, 5); //only return 5 in order
 					}).map((emoteWord) => {
 						return emotefxn.getEmoteImageStream(emoteWord);
 					}).then((imageStreams) => {
@@ -161,7 +157,7 @@ return readFile('./appstate.json', 'utf8')
 							attachment: imageStreams
 						}, message.threadID);
 					}).catch(function(err) {
-							console.error('Promise.all() threw an error!', err);
+						console.error('Promise.all() threw an error!', err);
 					});
 				}
 			}
