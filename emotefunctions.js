b1a65aaca038ecb6a1d09ceaffd1d88a3ac3bc61
Ptomerty <shua3.14@gmail.com>
2017-06-17 00:20:14 -0400

    didn't work, good to merge

--- emotefunctions.js
+++ emotefunctions.js
@@ -20,13 +20,7 @@ var downloadImage = function(url, pathname) {
 				return close(fd);
 			}).then(() => {
 				wget.download(url, pathname)
-					.on('error', function(err) {
-						if (err.code === 'EEXIST') {
-							resolve //file exists and has been downloaded
-						} else {
-							reject
-						}
-					})
+					.on('error', reject)
 					.on('end', resolve);
 			}).catch(err => {
 				reject(err);
