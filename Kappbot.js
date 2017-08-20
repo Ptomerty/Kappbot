d49a612a420d3e2a80d3ed84840503fae838104e
Ptomerty <Ptomerty@users.noreply.github.com>
2017-08-19 22:21:42 -0400

    Update to TwitchEmotes v3 API, refactor. (#5)
    * attempt to update global emotes to v3
    
    * new v3? 21mb rip
    
    * begin full refactor, dynamically create json files
    
    * dupe file
    
    * switch away from wget
    
    * remove legacy formatted JSON check
    
    * debug stuff
    
    * more debugging, not possible on local machine
    
    * make dlImage a promise
    
    * put pipePromise into dlImage directly
    
    * shift around, remove dlImage
    
    * remove clutter, finalize?
    
    * update gitignore
    
    * update sampleemote
    
    * forgot fetch dependency
    
    * ignore custom emotes, can remake with setup
    
    * getEmoteImageStream now in cfc
    
    * debug stuff
    
    * remove debug
    
    * remove fetch, prepare to merge
    
    * update README

--- Kappbot.js
+++ Kappbot.js
@@ -2,21 +2,16 @@
 const Promise = require('bluebird');
 const fs = require('fs');
 const login = Promise.promisify(require('facebook-chat-api'));
-const wget = require('wget-improved');
 const emotefxn = require('./emotefunctions.js');
 const cfc = require('./checkForCommands.js');
 
-const globalEmotes = require('./global.json');
-const subEmotes = require('./subs.json');
+const twitchEmotes = require('./twitch.json');
 const bttvEmotes = require('./bttv.json');
 const customEmotes = require('./custom.json');
 
 const readFile = Promise.promisify(fs.readFile);
-const writeFile = Promise.promisify(fs.writeFile);
-const unlink = Promise.promisify(fs.unlink);
 
 const appstatejson = require('./appstate.json')
-
 
 Promise.try(function() {
 	return readFile('./modlist', 'utf8')
@@ -35,12 +30,10 @@ Promise.try(function() {
 
 	api.listen((err, message) => {
 		if (err) return console.warn(err);
-
 		if (typeof message.body === 'string' && message.body !== undefined) {
 			cfc.parse(api, message);
 		}
-	}); //api.listen
-
+	});
 }).catch((err) => {
 	console.error("Error during login/connection to API!", err);
 }); //login
