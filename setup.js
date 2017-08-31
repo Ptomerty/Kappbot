e053bb342cb33c05376165b6a3d9764e7f6611f9
Ptomerty <Ptomerty@users.noreply.github.com>
2017-08-30 22:01:45 -0400

    update setup, fix promise.all error

--- setup.js
+++ setup.js
@@ -8,33 +8,54 @@ const writeFile = Promise.promisify(fs.writeFile);
 
 //DRY!
 
-Promise.try(function(){
-	return fetch('https://twitchemotes.com/api_cache/v3/images.json')
-}).then((res) => {
-	return res.json();
-}).then((json) => {
-	console.log("Fetched Twitch JSON!")
-	var newjson = {}
-	for (const key of Object.keys(json)) {
-	    	newjson[json[key].code.toLowerCase()] = key.toLowerCase();
+Promise.try(function() {
+	return ['https://api.betterttv.net/emotes','https://twitchemotes.com/api_cache/v3/images.json']
+}).map((url) => {
+	return fetch(url)
+}).map((urldata) => {
+	return urldata.json();
+}).then((jsons) => {
+	console.log("JSONS fetched!")
+	var bttvjson = {}
+	for (const key of Object.keys(jsons[0].emotes)) {
+    	bttvjson[jsons[0].emotes[key].regex.toLowerCase()] = jsons[0].emotes[key].url.toLowerCase().slice(-27, -3);
 	}
-	return writeFile('./twitch.json', JSON.stringify(newjson), 'utf8')
-}).then(() => {
-	console.log("Wrote Twitch JSON!")
-	return fetch('https://api.betterttv.net/emotes')
-}).then((res) => {
-	return res.json();
-}).then((json) => {
-	console.log("Fetched BTTV JSON!")
-	var newjson = {}
-	for (const key of Object.keys(json.emotes)) {
-    	newjson[json.emotes[key].regex.toLowerCase()] = json.emotes[key].url.toLowerCase().slice(-27, -3);
+	var twitchjson = {}
+	for (const key of Object.keys(jsons[1])) {
+    	twitchjson[jsons[1][key].code.toLowerCase()] = key.toLowerCase();
 	}
-	return writeFile('./bttv.json', JSON.stringify(newjson), 'utf8')
+	return [bttvjson, twitchjson];
+}).then(([a, b]) => {
+	return Promise.all([readFile("/usr/share/dict/words", "utf8"), readFile("./wordlist", "utf8"), [a, b]]); //return promise with jsons packaged
+}).then(([words, list, [a,b]]) => {
+	console.log("Dictionary and wordlist fetched!")
+	var dict = words.toString().toLowerCase().split("\n");
+	var wordlist = list.toString().toLowerCase().split("\n");
+	console.log(typeof dict)
+	dict.forEach(function(element) {
+		if (a[element] != null) {
+			console.log("deleting " + element + " at " + a[element] + " in bttv");
+			delete a[element];
+		}
+		if (b[element] != null) {
+			console.log("deleting " + element + " at " + b[element] + " in twitch");
+			delete b[element];
+		}
+	})
+	wordlist.forEach(function(element) {
+		if (a[element] != null) {
+			console.log("deleting " + element + " at " + a[element] + " in bttv");
+			delete a[element];
+		}
+		if (b[element] != null) {
+			console.log("deleting " + element + " at " + b[element] + " in twitch");
+			delete b[element];
+		}
+	})
+	return [a,b];
+}).then((jsons) => {
+	console.log("elements deleted!")
+	return Promise.all([writeFile('./bttv.json', JSON.stringify(jsons[0]), 'utf8'),	writeFile('./twitch.json', JSON.stringify(jsons[1]), 'utf8')]);
 }).then(() => {
-	let locations = ["/usr/share/dict/words", "./wordlist"];
-	readFile("/usr/share/dict/words", "utf8");
-}).then((data) => {
-	listOfCommonWords = data.toString().split("\n")
-	return console.log("Wrote BTTV JSON!") // error handling requires return all the way through
+	return console.log("Done!")
 })
