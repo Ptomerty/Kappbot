dcfcf3d8cdaee9495eaa4378a1fb9426b3bef95d
Ptomerty <Ptomerty@users.noreply.github.com>
2018-05-11 20:19:14 -0400

    why am i here

--- updateEmotes.sh
+++ updateEmotes.sh
@@ -5,7 +5,7 @@ git update-index --assume-unchanged modlist
 git update-index --assume-unchanged custom.json
 git reset checkForCommands.js
 git reset Kappbot.js
-git reset checkForCommands.js
+git checkout checkForCommands.js
 git checkout Kappbot.js
 git pull
 node setup.js
