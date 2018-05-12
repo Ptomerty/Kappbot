24b067a5d0283ae6e5ce61cbf8a83e2dc9fe34b7
Ptomerty <Ptomerty@users.noreply.github.com>
2018-05-11 20:14:14 -0400

    i love dirty history

--- updateEmotes.sh
+++ updateEmotes.sh
@@ -3,6 +3,10 @@
 forever stopall
 git update-index --assume-unchanged modlist
 git update-index --assume-unchanged custom.json
+git reset checkForCommands.js
+git reset Kappbot.js
+git reset checkForCommands.js
+git checkout Kappbot.js
 git pull
 node setup.js
 forever start Kappbot.js
